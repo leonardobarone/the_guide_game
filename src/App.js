709727d7ba90f33c3d7b87ff46f95b7c6aceb22a
unknown <leonardobarone26@gmail.com>
2024-12-16 10:42:30 +0100

    add _redirects in public

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <>
+    <h1>The Guide Game</h1>
+    <p>Il miglior gioco guida di tutti i tempi!</p>
+  </>;
+}
+
+export default App;
