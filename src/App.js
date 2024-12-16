f2d4dd2b31187367bc68f7ab982d425097c09052
unknown <leonardobarone26@gmail.com>
2024-12-16 17:20:50 +0100

    start google maps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+'use client';
+
+import {
+  APIProvider,
+  Map,
+  useMap,
+  AdvancedMarker,
+} from '@vis.gl/react-google-maps';
+
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+import { useEffect, useState, useRef } from 'react';
+import { Marker } from '@googlemaps/markerclusterer';
+import places from './places';
+
 // import logo from './logo.svg';
 import './App.css';
 
@@ -5,7 +19,22 @@ function App() {
   return <>
     <h1>The Guide Game</h1>
     <p>Il miglior gioco guida di tutti i tempi!</p>
+    <div style={{height: '80vh', width: '100%'}}>
+      <APIProvider apiKey='AIzaSyA7zTeRL20KTZpxmmgoSyDIBUZqgdxW1xk'>
+        <Map 
+          center={{lat: 40.7564296094562, lng: 14.013971500161295}} 
+          zoom={13} 
+          mapId={'b28b9e40a7377c78'}
+        >
+          <Markers places={places} />
+        </Map>
+      </APIProvider>
+    </div>
   </>;
 }
 
 export default App;
+
+const Markers = ({ places }) => {
+  return null;
+}
