388aed220ad641975b6d9cefb8d67f88e38c1362
unknown <leonardobarone26@gmail.com>
2024-12-23 10:47:37 +0100

    fix

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -14,6 +14,10 @@ const Wrapper = styled.nav`
     position: absolute;
     bottom: 0px;
     left: 0px;
+    width: 100%;
+    position: fixed;
+    padding: 16px;
+    z-index: 10;
     background-color: red;
 `;
 
