07e4648b57524721a8acb974c982ec4388e9ea95
unknown <leonardobarone26@gmail.com>
2025-01-08 23:24:22 +0100

    fix quiz

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,13 +1,52 @@
+import styled from 'styled-components';
+
 const Quiz = () => {
-  return (
-    <div style={{padding: '100px 33px'}}>
+  return <Wrapper>
+        <div style={{padding: '100px 33px'}}>
       <h4>Proviamo un quiz con un input</h4>
       <form action="">
-        <label htmlFor="prova">Prova</label>
-        <input id="prova" name="prova" type="text" />
+        <div>
+          <label htmlFor="nome">nome</label>
+          <input id="nome" name="nome" type="text" />
+        </div>
+        <div>
+          <label htmlFor="cognome">cognome</label>
+          <input id="cognome" name="cognome" type="text" />
+        </div>
+        <button type="submit">Prova</button>       
       </form>
     </div>
-  )
+  </Wrapper>
 }
 
 export default Quiz;
+
+
+// for = htmlFor della label (corrisponde all'id) 
+
+
+const Wrapper = styled.section`
+    h4 {
+      text-align: center;
+    }
+    form {
+      margin-top: 10px;
+    }
+    button {
+      margin-top: 10px;
+      padding: 8px 20px;
+      background-color: lawngreen;
+      color: white;
+      border-radius: 5px;
+      border-style: none;
+      font-weight: bold;
+    }
+    button:focus, button:hover, button:active {
+      background-color: green;
+    }
+    form div {
+      padding: 5px 0px;
+      display: flex;
+      justify-content: space-between;
+    }
+`;
