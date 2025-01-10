7163c80e61f35310d2e0e036a8a969d5689f0c89
unknown <leonardobarone26@gmail.com>
2025-01-10 16:09:16 +0100

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,42 @@ import background from '../images/background.jpg';
 
 const Home = () => {
     return (
-      <div style={{padding: '32px', height: 'calc(100vh - 150.41px)', width: '100%', height: 'calc(100vh - 150.41px)', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
-        <h1>Pagina Home</h1>
-        <em>Notte ultimo aggiornamento: 09/01/2025</em>
-        <hr style={{margin: '10px 0px'}} />
-        <h3>Cose modificate:</h3>
-        <ul>
-            <li>Precisato i pulsanti in basso</li>
-            <li>Creata pagina home</li>
-            <li>Cliccare Game of Procida per tornare nella Home</li>
-            <li>Create pagine vuote e ordinate</li>
-            <li>Il quiz sta nella posizione: Palazzo Merlato</li>
-            <li>
-                <Link to='/quiz'>Puoi anche cliccare qui per andare direttamente al gioco</Link>
-            </li>
-        </ul>
-      </div>
+      <div style={{padding: '32px', width: '100%', overflow: 'scroll', height: 'calc(100vh - 150.41px)', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
+
+          <h1>Pagina Home</h1>
+          <em>Notte ultimo aggiornamento: 09/01/2025</em>
+          <hr style={{margin: '10px 0px'}} />
+          <h3>Cose modificate:</h3>
+          <ul>
+              <li>Precisato i pulsanti in basso</li>
+              <li>Creata pagina home</li>
+              <li>Cliccare Game of Procida per tornare nella Home</li>
+              <li>Create pagine vuote e ordinate</li>
+              <li>Il quiz sta nella posizione: Palazzo Merlato</li>
+              <li>
+                  <Link to='/quiz'>Puoi anche cliccare qui per andare direttamente al gioco</Link>
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptates, rerum nemo reprehenderit illum quidem voluptatibus ipsa ab quo placeat consequatur exercitationem quasi at recusandae perspiciatis doloremque numquam, perferendis omnis!
+              </li>
+          </ul>
+        </div>
+
     )
   }
   
