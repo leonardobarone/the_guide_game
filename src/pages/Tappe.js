dff2883a40e15afcc9e149695690536b70ababa9
unknown <leonardobarone26@gmail.com>
2025-01-13 15:38:15 +0100

    create card in tappe

--- src/pages/Tappe.js
+++ src/pages/Tappe.js
@@ -1,25 +1,48 @@
+import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import places from "../utils/places";
 
 const Tappe = () => {
-    return (
-      <div style={{padding: '32px'}}>
-        <h1>Pagina Tappe</h1>
-        <ul>
-          {
-            places.map((place) => {
-              return <li key={place.id}>
-                {place.name}
-
-                {place.game ? <Link to={place.game}>VAI AL GIOCO</Link> : ''}
-              
-              </li>   
-            })
-          }
-        </ul>
-      </div>
-    )
+    return <Wrapper>
+      {places.map((place) => {
+        return <div className='containerCard'>
+          <div className="card">
+            <div className="left">
+              <img src={place.img ? place.img : 'https://static.vecteezy.com/system/resources/thumbnails/008/695/917/small_2x/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg'} alt="" />
+            </div>
+            <div className="right">
+              <h5>{place.name.toUpperCase()}</h5>
+            </div>
+          </div>
+        </div>
+      })}
+    </Wrapper>
   }
   
   export default Tappe;
   
+  const Wrapper = styled.section`
+
+  .containerCard {
+    padding: 15px;
+    .card {
+      padding: 3px;
+      border: 1px solid black;
+      display: flex;
+      justify-content: space-between;
+    }
+    img {
+      width: 100px;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+    .left {
+      width: 30%;
+    }
+    .right {
+      padding: 10px;
+      width: 70%;
+    }
+  }
+  `
