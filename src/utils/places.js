ca8ca2989775cef4d84aa0edfa7bb1b8e73db4e6
unknown <leonardobarone26@gmail.com>
2025-01-19 22:23:17 +0100

    nel file places.js ho cambiato gli id da number a string perché dal parametro arriva una stringa e non un numero e react chiede un'uguaglianza anche di tipo

--- src/utils/places.js
+++ src/utils/places.js
@@ -18,92 +18,92 @@ import cimitero from '../images/places/cimitero.png';
 
 const places = [
     { 
-      'id' : 1,
+      'id' : "1",
       'name' : 'vivara', 
       'img' : vivara,
       'risolto' : true
     },
     { 
-      'id' : 2,
+      'id' : "2",
       'name' : 'chiesa chiaiolella',
       'img' : diFronteSanGiuseppe,
       'risolto' : true
     },
     { 
-      'id' : 3,
+      'id' : "3",
       'name' : 'belvedere elsa morante', 
       'img' : belvedereElsa,
       'risolto' : true
     },
     { 
-      'id' : 4,
+      'id' : "4",
       'name' : 'san giacomo', 
       'img' : sanGiacomo,
       'risolto' : true
     },
     { 
-      'id' : 5,
+      'id' : "5",
       'name' : 'chiesa san leonardo', 
       'img' : sanLeonardo,
       'risolto' : true
     },
     { 
-      'id' : 6,
+      'id' : "6",
       'name' : 'chiesa madonna delle grazie', 
       'img' : piazzaMartiri,
       'risolto' : true
     },
     { 
-      'id' : 7,
+      'id' : "7",
       'name' : 'santa margherita nuova', 
       'img' : cannoni,
       'risolto' : false
     },
     { 
-      'id' : 8,
+      'id' : "8",
       'name' : 'palazzo d\'avalos', 
       'img' : piazzaArmi,
       'risolto' : true
     },
     { 
-      'id' : 9,
+      'id' : "9",
       'name' : 'palazzo della cultura', 
       'img' : palazzoCultura,
       'risolto' : true
     },
     { 
-      'id' : 10,
+      'id' : "10",
       'name' : 'abbazia di san michele arcangelo', 
       'img' : sanMichele,
       'risolto' : true
     },
     { 
-      'id' : 11,
+      'id' : "11",
       'name' : 'marina corricella', 
       'img' : corricella,
       'risolto' : true
     },
     { 
-      'id' : 12,
+      'id' : "12",
       'name' : 'Palazzo Merlato', 
       'img' : palazzoMerlato,
       'game' : '/quiz',
       'risolto' : true
     },
     { 
-      'id' : 13,
+      'id' : "13",
       'name' : 'chiesa marina grande', 
       'img' : marinaChiesa,
       'risolto' : true
     },
     { 
-      'id' : 14,
+      'id' : "14",
       'name' : 'faro', 
       'img' : faro,
       'risolto' : true
     },
     { 
-      'id' : 15,
+      'id' : "15",
       'name' : 'pozzo vecchio', 
       'img' : cimitero,
       'risolto' : true
