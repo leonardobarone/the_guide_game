5bf7dfe53207de49f147262191d3ebb1ebeca6c5
unknown <leonardobarone26@gmail.com>
2025-01-30 12:08:14 +0100

    fix

--- src/pages/navbar/Place.js
+++ src/pages/navbar/Place.js
@@ -6,27 +6,71 @@ const Place = () => {
 
     const {places} = useGlobalContext();
 
-    return <Wrapper style={{marginTop: '50px', marginBottom: '75px',}}>
+    return <Wrapper>
       {places.map((place) => {
-        return <div key={place.id} className='containerCard'>
-          <Link to={`/luoghi/${place.unblocked ? place.id : ''}`} className="card link">
-            <div className="left">
+        return <div key={place.id} className="cardOutside">
+          <div className="cardInside">
+            <div className="imgContainer">
               <img src={place.unblocked ? place.imgVisible : place.imgHidden} alt="" />
             </div>
-            <div className="right">
-              <h5>{place.unblocked ? place.name.toUpperCase() : 'BLOCCATO'}</h5>
-            </div>
-          </Link>
+            <h3 style={{backgroundColor: `${place.unblocked ? 'var(--success-bootstrap)' : 'var(--danger-bootstrap)'}`}}>{place.unblocked ? place.name : 'BLOCCATO'}</h3>
+          </div>
         </div>
+        // return <div key={place.id} className='containerCard'>
+        //   <Link to={`/luoghi/${place.unblocked ? place.id : ''}`} className="card link">
+        //     <div className="left">
+        //       <img src={place.unblocked ? place.imgVisible : place.imgHidden} alt="" />
+        //     </div>
+        //     <div className="right">
+        //       <h5>{place.unblocked ? place.name.toUpperCase() : 'BLOCCATO'}</h5>
+        //     </div>
+        //   </Link>
+        // </div>
       })}
     </Wrapper>
   }
   
   export default Place;
   
-  const Wrapper = styled.main`
+  const Wrapper = styled.section`
+  margin-top: 60px;
+  margin-bottom: 80px;
+  height: calc(100% - 140px);
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
 
-  .containerCard {
+  .cardOutside {
+    flex-basis: 50%;
+    padding: 10px;
+  }
+  
+  .cardInside {
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    .imgContainer {
+      margin-bottom: 8px;
+    }
+    img {
+      display: block;
+      width: 85%;
+      margin: 0 auto;
+    }
+    h3 {
+      padding: 7px;
+      text-align: center;
+      text-transform: uppercase;
+      font-size: 10px;
+      border-radius: 15px;
+      color: white;
+    }
+  }
+
+
+
+  /* .containerCard {
     padding: 10px 10px 0px 10px;
     &:last-child {
       padding: 10px;
@@ -70,5 +114,5 @@ const Place = () => {
       padding: 10px;
       width: 75%;
     }
-  }
+  } */
   `
