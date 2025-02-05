5e5f21257f24e45f20b91df7c70fa98cb4fef3be
unknown <leonardobarone26@gmail.com>
2025-02-05 17:30:28 +0100

    x

--- src/pages/navbar/Account.js
+++ src/pages/navbar/Account.js
@@ -1,21 +1,11 @@
 import styled from "styled-components";
-import { MdOutlineArrowBack } from "react-icons/md";
-import { IoIosNotifications } from "react-icons/io";
+import waveTop from '../../images/waveTop.svg';
+
 
 const Account = () => {
     return (
       <Wrapper>
-        <div className="top">
-          <div className="left">
-              <MdOutlineArrowBack className="arrowLeft" />
-            <div className="nameAccount">
-              Leonardo
-            </div>
-          </div>
-          <div className="right">
-            <IoIosNotifications className="arrowRight" />
-          </div>
-        </div>
+        
       </Wrapper>
     )
   }
@@ -23,43 +13,21 @@ const Account = () => {
 export default Account;
   
 const Wrapper = styled.main`
-  margin-top: 60px;
-  margin-bottom: 75px;
-  height: calc(100vh - 140px);
-  .top {
-    padding: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    .left {
-      display: flex;
-      align-items: center;
-      .arrowLeft {
-        height: 30px;
-        width: 30px;
-        border: 1px solid black;
-        font-size: 25px;
-        padding: 3px;
-        border-radius: 100%;
-      }
-      .nameAccount {
-        height: 30px;
-        margin-left: 10px;
-        padding-top: 3px;
-        font-size: 22px;
-        font-weight: bold;
-      }
-    }
-    .right {
-      .arrowRight {
-        display: block;
-        height: 30px;
-        width: 30px;
-        border: 1px solid black;
-        font-size: 25px;
-        padding: 3px;
-        border-radius: 100%;
-      }
-    }
+  background-color: var(--gray);
+  margin-top: 40px;
+  margin-bottom: 60px;
+  position: relative;
+  height: 300px;
+  &::after {
+    content: '';
+    background-image: url(${waveTop});
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    bottom: -25px;
+    left: 0px;
+    height: 25px;
+    position: absolute;
   }
 `
