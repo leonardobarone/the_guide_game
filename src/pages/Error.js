720b4a918d6ef90b54cba9b0c7b6772b91111ef1
unknown <leonardobarone26@gmail.com>
2025-02-07 12:12:02 +0100

    fix

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,15 @@
+import Title from "../components/Title";
+import styled from "styled-components";
+
 const Error = () => {
-  return (
-    <div style={{marginTop: '50px', marginBottom: '75px', padding: '32px'}}>
-      <h1>404 - Pagina Non Trovata</h1>
-    </div>
-  )
+  return <Wrapper>
+    <Title one={'Page Not Found'} two={'404'} />
+  </Wrapper>
 }
 
 export default Error;
+
+const Wrapper = styled.main`
+  margin-top: 40px;
+  margin-bottom: 60px;
+`
--- src/pages/navbar/PlaceItem.js
+++ src/pages/navbar/PlaceItem.js
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../../context";
 import grid from '../../images/grid.svg'
+import Error from "../Error";
 
 const PlaceItem = () => {
   const {places} = useGlobalContext();
   const {id} = useParams();
   const oggettoTrovato = places.find(oggetto => oggetto.id === id);
   
+  if (!oggettoTrovato) {
+    return <Error />
+  } 
+
   return (
     <div style={{marginTop: '50px', marginBottom: '75px'}}>
       <img width={'250'} src={oggettoTrovato.imgVisible} alt="" />
