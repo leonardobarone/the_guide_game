98fb7f91167101820305901404495686a693cb93
Leonardo <leonardobarone26@gmail.com>
2025-03-24 09:50:31 +0100

    init doors!

--- src/pages/games/Porte.js
+++ src/pages/games/Porte.js
@@ -1,9 +1,34 @@
 import styled from "styled-components"
+import { useState, useEffect } from "react"
 
 const Porte = () => {
+
+  const [result, setResult] = useState('')
+  const items = Array(6).fill(null);
+
+  const formaRisultato = (value) => {
+    setResult(prev => prev + value);
+  }
+  useEffect(() => {
+    console.log(result)
+  }, [result])
   return (
     <Wrapper>
-        Porte
+        <div className="top">
+          <div>{result[0] ? result[0] : 'no'}</div>
+          <div>{result[1] ? result[1] : 'no'}</div>
+          <div>{result[2] ? result[2] : 'no'}</div>
+          <div>{result[3] ? result[3] : 'no'}</div>
+          <div>{result[4] ? result[4] : 'no'}</div>
+          <div>{result[5] ? result[5] : 'no'}</div>
+        </div>
+        <div className="bottom">
+          {
+            items.map((_, index) => {
+              return <div onClick={() => formaRisultato(index + 1)} key={index + 1}>{index + 1}</div>
+            })          
+          }
+        </div>
     </Wrapper>
   )
 }
@@ -12,4 +37,15 @@ export default Porte
 
 const Wrapper = styled.section`
     margin-top: 50px;
+    .top, .bottom {
+      border: 1px solid black;
+      display: flex;
+      div {
+        border: 1px solid red;
+        width: calc(100% / 6);
+        text-align: center;
+      }
+    }
+    
+
 `
