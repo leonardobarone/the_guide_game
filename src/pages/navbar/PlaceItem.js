660d9105aebc0f0d84d5ea1e9d1afb8d850541b9
unknown <leonardobarone26@gmail.com>
2025-04-12 15:43:12 +0200

    fix

--- src/pages/navbar/PlaceItem.js
+++ src/pages/navbar/PlaceItem.js
@@ -3,15 +3,21 @@ import { useGlobalContext } from "../../context";
 import Error from "../Error";
 import styled from "styled-components";
 import Title from "../../components/Title";
+import useImagesLoaded from '../../utils/useImagesLoaded';
+import Loading from '../../components/Loading';
 
 const PlaceItem = () => {
   const {places} = useGlobalContext();
   const {id} = useParams();
   const oggettoTrovato = places.find(oggetto => oggetto.id === id && oggetto.unblocked);
-  
+  const images = [oggettoTrovato.cover];
+  const loading = useImagesLoaded(images);
+
   if (!oggettoTrovato) {
     return <Error />
   } 
+
+  if (!loading) return <Loading height={110}/>
 
   return (
     <Wrapper style={{marginTop: '50px', marginBottom: '75px'}}>
